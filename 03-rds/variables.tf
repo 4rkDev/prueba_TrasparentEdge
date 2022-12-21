a12f88481593cd0652114b93d1e4bc50012e38b4
Jonay Marrero <jmarrero@nakytec.com>
2022-12-21 00:15:42 +0000

    create eks

---
+++ 03-rds/variables.tf
@@ -0,0 +1,16 @@
+
+variable "db_password" {
+  description = "Password for the RDS database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_username" {
+  description = "Username for the RDS database"
+  type        = string
+  sensitive = true
+  
+}
+
+
+
